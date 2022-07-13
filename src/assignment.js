9ce86301ef8b4e109a2336d23002e0a91fb87f3a
Adebanjo Abolanle Mary <37559038+Kaempy@users.noreply.github.com>
2022-07-13 22:02:47 +0100

    Update assignment.js

--- src/assignment.js
+++ src/assignment.js
@@ -5,29 +5,17 @@ const assignment = {};
 
 /**
  * Challenge - 1
- *
+ * 
  * Calculate and return the sum of the numbers in an array.
  * If you did Challenge - 1, remove the comment in the line just after this function
- *
+ * 
  * @param {Array} arrayOfNumbers the array of numbers to sum
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-
-    let totalSum = 0;
-    for (const index of arrayOfNumbers) {
-        totalSum += index
-    }
-    return totalSum
-
-    // let initialValue  = 0 
-    // const sum = arrayOfNumbers.reduce(
-    //     (prevValue, currentValue) => prevValue + currentValue,
-    //     initialValue
-    // );
-    // return sum;
+    return arrayOfNumbers.length;
 }
-assignment.sumOfNumbers = sumOfNumbers;
+// assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
